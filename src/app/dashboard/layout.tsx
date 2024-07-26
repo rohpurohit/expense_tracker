10227c3551825053a0aca68d38ca68e54c9fa6e6
rohan <rohan.purohit@coditation.com>
2024-07-26 23:33:07 +0530

    init commit

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,12 @@
+import SideNav from "../../components/ui/SideNav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="flex bg-muted/40">
+        <SideNav />
+      </div>
+      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
